501b256da185abe2f4c30b24a88c9c6c1d298115
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-12-16 01:59:54 +0100

    implementato DTO Generico utilizzabile per api che forniscono come risposta un singolo attributo (un messaggio o un esito di un operazione)

---
+++ src/main/java/com/luca/engineer/dto/MessaggioGenericoResponse.java
@@ -0,0 +1,14 @@
+package com.luca.engineer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MessaggioGenericoResponse {
+
+	private Object messaggio;
+	
+}
